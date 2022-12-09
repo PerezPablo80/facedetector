922adf09cad8f95994dd8f81febec8fd5187d3de
Pablo Perez <perez.pablo@gmail.com>
2022-12-09 09:25:25 -0300

    get rid of node backend and only process python backend

--- python/server.py
+++ python/server.py
@@ -3,6 +3,7 @@ import threading
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect
 import facedetector
+import file_handler
 load_dotenv()
 # test= os.getenv("TEST")
 
@@ -16,21 +17,34 @@ def init():
     return {"Status": "true", "message": " all ok"}
 
 
-@app.route('/file', methods=['PUT'])
-def putter():
-    try:
-        previousName = previousFolder+"/"+request.json['previousName']
-        actualName = actualFolder+"/"+request.json['actualName']
-        if (os.path.isfile(previousName) and not os.path.isfile(actualName)):
-            os.rename(previousName, actualName)
-        else:
-            return {"status": "false", "message": "actualizacion incorrecta, archivo a actualizar inexistente o nombre nuevo existente"}
-        return {"status": "true", "message": "actualizacion correcta"}
-    except Exception as e:
-        print(e)
-        return {"status": "false", "message": "actualizacion incorrecta"}
+@app.route('/file', methods=['PUT', 'GET'])
+def actions():
+    if request.method == 'GET':
+        return file_handler.listFiles(actualFolder)
+    elif request.method == 'PUT':
+        previousFile = previousFolder+"/"+request.json['previousName']
+        actualFile = actualFolder+"/"+request.json['actualName']
+        return file_handler.update(previousFile, actualFile)
+
+
+@app.route('/emptyFolder', methods=['PUT', 'GET'])
+def folder():
+    return file_handler.emptyFolder()
 
 # app.run()
+
+# def putter():
+#     try:
+#         previousName = previousFolder+"/"+request.json['previousName']
+#         actualName = actualFolder+"/"+request.json['actualName']
+#         if (os.path.isfile(previousName) and not os.path.isfile(actualName)):
+#             os.rename(previousName, actualName)
+#         else:
+#             return {"status": "false", "message": "actualizacion incorrecta, archivo a actualizar inexistente o nombre nuevo existente"}
+#         return {"status": "true", "message": "actualizacion correcta"}
+#     except Exception as e:
+#         print(e)
+#         return {"status": "false", "message": "actualizacion incorrecta"}
 
 
 def fcinit():
